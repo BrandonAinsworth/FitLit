d88c73e9a057f8512ca3a1c7e4624c8daf881e34
Rachel Bock <rachel.online@gmail.com>
2022-05-16 19:23:58 -0600

    Add constructor and returnSpecificUser functionality to UserRepository class" `

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,4 +1,32 @@
 class UserRepository {
+  constructor(dataDetails) {
+    this.users = dataDetails.users;
+    this.usersActivity = dataDetails.activityData;
+    this.usersHydration = dataDetails.hydrationData;
+    this.usersSleep = dataDetails.sleepData;
+    this.averageStepGoal = () => {
+
+    } 
+    this.averageSleepQuality = () => {
+
+    }
+  }
+
+  returnSpecificUser(userID) {
+    // We want to input the userID and output the user object.
+
+    // We will need to pull the user information from each of the four 
+    // datasets.
+
+    let outputUser = {};
+    outputUser.users = this.users.filter((user) => user.id === userID);
+    outputUser.activityData = this.usersActivity.filter((user) => user.userID === userID);
+    outputUser.hydrationData = this.usersHydration.filter((user) => user.userID === userID);
+    outputUser.sleepData = this.usersSleep.filter((user) => user.userID === userID);
+    
+    console.log(outputUser);
+    return outputUser;
+  }
 
 }
 
