543d2d5d305ea2bb1a8801ac023a7120123bbfa6
Rachel Bock <rachel.online@gmail.com>
2022-05-18 10:06:29 -0600

    Add UserRepository class

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,36 +1,21 @@
 class UserRepository {
-  constructor(dataDetails) {
-    this.users = dataDetails.users;
-    this.usersActivity = dataDetails.activityData;
-    this.usersHydration = dataDetails.hydrationData;
-    this.usersSleep = dataDetails.sleepData;
-    this.averageStepGoalAllUsers = () => {
-      let result = 0;
-      let totalStepsGoal = this.users.reduce((total, stepDataPoint) => {
-
-        total += stepDataPoint.dailyStepGoal;
-        return total;
-      }, 0);
-      result = parseInt((totalStepsGoal / this.users.length).toFixed(0));
-      return result;
-    } 
-    this.averageSleepQuality = () => {
-
-    }
+  constructor(data) {
+    this.usersData = data;
   }
 
-  returnSpecificUser(userID) {
-    // We want to input the userID and output the user object.
+  returnSpecificUser(id) {
+    return this.usersData.find((user) => id === user.id );
+  }
 
-    // We will need to pull the user information from each of the four 
-    // datasets.
+  averageStepGoalAllUsers() {
+    let result = 0;
+    let totalStepsGoal = this.usersData.reduce((total, user) => {
 
-    let outputUser = {};
-    outputUser.users = this.users.filter((user) => user.id === userID);
-    outputUser.activityData = this.usersActivity.filter((user) => user.userID === userID);
-    outputUser.hydrationData = this.usersHydration.filter((user) => user.userID === userID);
-    outputUser.sleepData = this.usersSleep.filter((user) => user.userID === userID);
-    return outputUser;
+        total += user.dailyStepGoal;
+        return total;
+      }, 0);
+      result = parseInt((totalStepsGoal / this.usersData.length).toFixed(0));
+      return result;
   }
 
 }
