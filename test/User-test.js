a08381261287f784ce55a55197910509fee1faab
Amber Bodnar <amber.bodnar@gmail.com>
2022-05-20 16:12:04 -0400

    Add sample hydration data to user test

--- test/User-test.js
+++ test/User-test.js
@@ -1,43 +1,90 @@
 import { expect } from 'chai';
 import User from '../src/User';
+import Hydration from '../src/Hydration';
 
-describe('User', () => { 
-  let user1;
-  let luisa;
+describe('User', () => {
+  let user;
+  let currentUser;
+  let currentUserHydration;
 
   beforeEach(() => {
-    user1 =  [{
-      "id": 1,
-      "name": "Luisa Hane",
-      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-      "email": "Diana.Hayes1@hotmail.com",
-      "strideLength": 4.3,
-      "dailyStepGoal": 10000,
+    user =  [
+    {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
       "friends": [
-        16,
-        4,
-        8
+        9,
+        18,
+        24,
+        19
       ]
-    }]
+    },
+  ];
 
-    luisa = new User(user1);
-  }); 
+    currentUserHydration = [
+    {
+      "userID": 2,
+      "date": "2019/06/15",
+      "numOunces": 75
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/16",
+      "numOunces": 91
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/17",
+      "numOunces": 96
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/18",
+      "numOunces": 70
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/19",
+      "numOunces": 76
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/20",
+      "numOunces": 71
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/21",
+      "numOunces": 27
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/22",
+      "numOunces": 58
+    }
+  ];
+
+    currentUser = new User(user);
+  });
 
   it('should be a function', function () {
     expect(User).to.be.a('function');
   });
 
   it(`should be an instance of User`, () => {
-    expect(luisa).to.be.instanceOf(User);
-    
+    expect(currentUser).to.be.instanceOf(User);
   });
 
-  it(`should be equal to user1 data`, () => {
-    expect(luisa.user).to.deep.equal(user1[0]);
+  it(`should be equal to user data`, () => {
+    expect(currentUser.user).to.deep.equal(user[0]);
   });
-  
+
   it(`should return a first name of the user`, () => {
-    expect(luisa.returnUserFirstName()).to.be.equal('Luisa')
+    expect(currentUser.returnUserFirstName()).to.be.equal('Jarvis')
   });
 
 });
