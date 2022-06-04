b84893270d8ef46a25625585a2035f791aed10de
Amber Bodnar <amber.bodnar@gmail.com>
2022-06-03 21:14:07 -0400

    Update fetch request with new api url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
 export function fetchData(type) {
-  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${type}`)
+  return fetch(`http://localhost:3001/api/v1/${type}`)
     .then(response => checkForErrors(response));
 }
 
-let promise = Promise.all([fetchData('users'), fetchData('sleep'), fetchData('hydration')]);
+let promise = Promise.all([fetchData('users'), fetchData('sleep'), fetchData('hydration'), fetchData('activity')]);
 
 function checkForErrors(response) {
   if (response.ok) {
